d10baed9455ea79fd08f6d9b295c504bb1b3083b
CVFireDragon <cowardlyvids@gmail.com>
2019-08-11 13:34:29 -0400

    add whitespace

--- szstoolgui.py
+++ szstoolgui.py
@@ -25,7 +25,7 @@
 ################################################################
 ################################################################
 
-
+from szstool import extract
 from szstool import pack
 
 import platform
@@ -69,63 +69,6 @@ def center(win, offx=0, offy=0):
     x = (win.winfo_screenwidth() // 2) - (width // 2)
     y = (win.winfo_screenheight() // 2) - (height // 2)
     win.geometry('{}x{}+{}+{}'.format(width, height, (x+offx), (y+offy)))
-    
-    def extract(file):
-    
-    with open(file, "rb") as inf:
-        inb = inf.read()
-
-    while yazLib.IsYazCompressed(inb):
-        inb = yazLib.decompress(inb)
-
-    name = os.path.splitext(file)[0]
-    ext = szsLib.guessFileExt(inb)
-
-    if ext != ".sarc":
-        with open(''.join([name, ext]), "wb") as out:
-            out.write(inb)
-
-    else:
-        arc = szsLib.SARC_Archive()
-        arc.load(inb)
-
-        root = os.path.join(os.path.dirname(file), name)
-        if not os.path.isdir(root):
-            os.mkdir(root)
-
-        files = []
-
-        def getAbsPath(folder, path):
-            nonlocal root
-            nonlocal files
-
-            for checkObj in folder.contents:
-                if isinstance(checkObj, szsLib.File):
-                    files.append(["/".join([path, checkObj.name]), checkObj.data])
-
-                else:
-                    path_ = os.path.join(root, "/".join([path, checkObj.name]))
-                    if not os.path.isdir(path_):
-                        os.mkdir(path_)
-
-                    getAbsPath(checkObj, "/".join([path, checkObj.name]))
-
-        for checkObj in arc.contents:
-            if isinstance(checkObj, szsLib.File):
-                files.append([checkObj.name, checkObj.data])
-
-            else:
-                path = os.path.join(root, checkObj.name)
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-
-                getAbsPath(checkObj, checkObj.name)
-
-        for file, fileData in files:
-            print(file)
-            with open(os.path.join(root, file), "wb") as out:
-                out.write(fileData)
-
 
 
 
